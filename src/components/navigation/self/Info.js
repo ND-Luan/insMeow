8b95af1afbefa4eee599d0c521fa40079a061a85
ngodinhvu123 <ngodinhvuluan@gmail.com>
2022-12-16 11:28:37 +0700

    fix menuPopUp Self

--- src/components/navigation/self/Info.js
+++ src/components/navigation/self/Info.js
@@ -1,6 +1,6 @@
 import {Text} from 'react-native';
 
-function Info() {
-  return <Text style={{padding: 20}}>Myself</Text>;
+function Info({title}) {
+  return <Text style={{padding: 20}}>{title}</Text>;
 }
 export default Info;
