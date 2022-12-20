03c38313104430f21b31b0297e567af3afaab9cb
ngodinhvu123 <ngodinhvuluan@gmail.com>
2022-12-20 10:19:28 +0700

    fix animated

--- src/animation/self/LineAnimation.js
+++ src/animation/self/LineAnimation.js
@@ -1,4 +1,17 @@
+import {useEffect, useRef} from 'react';
+import {Animated, Easing} from 'react-native';
+
 function LineAnimation() {
-  return;
+  const motion = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.timing(motion, {
+      toValue: 500,
+      duration: 1000,
+      useNativeDriver: false,
+      easing: Easing.ease,
+    });
+  }, []);
+
+  return <Animated.View></Animated.View>;
 }
 export default LineAnimation;
